467bca698e4e8f172bd958d484f5c4d116abd880
kixixixixi <kixixixixi@gmail.com>
2026-05-11 16:26:57 +0900

    Add event

--- components/events-map.tsx
+++ components/events-map.tsx
@@ -43,6 +43,7 @@ const venuePoints: Record<string, VenuePoint> = {
   "コラボカフェ本舗 秋葉原店": { lat: 35.7003, lng: 139.7715 },
   "キュアメイドカフェ": { lat: 35.6982, lng: 139.771 },
   "AKIHABARAゲーマーズ本店": { lat: 35.699, lng: 139.7714 },
+  "秋葉原Venus": { lat: 35.701, lng: 139.7716 },
   "アニメイト秋葉原2号館 7F": { lat: 35.6997, lng: 139.7714 },
   "アニメイト秋葉原ANNEX": { lat: 35.7001, lng: 139.7714 },
   "TOPPA!!! BASE AKIBA": { lat: 35.6984, lng: 139.773 },
