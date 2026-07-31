46020042553be0691d419b4e2d53b6faa097d7e0
kixixixixi <kixixixixi@gmail.com>
2026-08-01 08:48:07 +0900

    Add article

--- lib/venue-points.ts
+++ lib/venue-points.ts
@@ -120,6 +120,7 @@ export const venuePoints: Record<string, VenuePoint> = {
   "晴れる屋2 秋葉原タワー店": { lat: 35.6989, lng: 139.7716 },
   "クルクル秋葉原店": { lat: 35.7025, lng: 139.7715 },
   "Fandom Akiba 5F": { lat: 35.6989, lng: 139.7726 },
+  "刀剣乱舞STORE AKIHABARA": { lat: 35.7002, lng: 139.7716 },
 }
 
 export const getVenuePoint = (venue: string): VenuePoint | undefined => {
