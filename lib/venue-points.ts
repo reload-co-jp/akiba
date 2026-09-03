7cf8ce0e04d9af48cee61eaf2a4523b4517eb349
kixixixixi <kixixixixi@gmail.com>
2026-09-03 15:22:11 +0900

    Add article

--- lib/venue-points.ts
+++ lib/venue-points.ts
@@ -137,6 +137,7 @@ export const venuePoints: Record<string, VenuePoint> = {
   "d/style秋葉原店": { lat: 35.6989, lng: 139.7714 },
   "GOTO秋葉原ビル": { lat: 35.6973, lng: 139.7748 },
   "駿河屋秋葉原ゲーム館": { lat: 35.7024, lng: 139.7715 },
+  "居酒屋食事処 あだち": { lat: 35.6986, lng: 139.7723 },
 }
 
 export const getVenuePoint = (venue: string): VenuePoint | undefined => {
