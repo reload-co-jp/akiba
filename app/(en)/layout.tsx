74efad31617f38f96b52d450c1db3d7f53d00e08
kixixixixi <kixixixixi@gmail.com>
2026-09-09 23:19:33 +0900

    Add AdsenseRouteNotifier

--- app/(en)/layout.tsx
+++ app/(en)/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import Script from "next/script"
 import { absoluteUrl, siteDescriptionEn, siteNameEn, siteUrl } from "lib/site"
 import { LanguageProvider } from "components/language-provider"
+import { AdsenseRouteNotifier } from "components/adsense-route-notifier"
 import "../reset.css"
 
 const googleAnalyticsId = "G-ZJM8E54KXG"
@@ -73,6 +74,7 @@ const EnLayout = ({ children }: { children: React.ReactNode }) => {
       </head>
       <body>
         <LanguageProvider>
+          <AdsenseRouteNotifier />
           {isProduction && (
             <>
               <Script
